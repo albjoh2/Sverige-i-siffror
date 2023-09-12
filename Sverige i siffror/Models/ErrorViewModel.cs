0bc0f2b380965aa98b652758ed3a92b287ca100b
albjoh <aljo940616@gmail.com>
2023-09-12 21:01:45 +0200

    first commit

---
+++ Sverige i siffror/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Sverige_i_siffror.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
